b738e7cc9a9c0b2569d4ade9c8525fe1c711d1b1
Rashed Doha <RashedDoha92@gmail.com>
2016-08-18 00:50:55 +0600

    Added responsive hero text

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,10 @@ var roboarm = document.getElementById('roboarm');
 window.onload = function() {
     roboarm.classList.add("animate");
     TweenMax.to("#arm", 1, {
-        rotation: 15,
+        rotation: 16,
         delay: 4,
         transformOrigin: "20px 264px"
     });
+
 
 }
