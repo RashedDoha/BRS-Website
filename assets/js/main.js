4d0c0bb330919aece7ddaffc2011cbe7ad88aeca
Rashed Doha <RashedDoha92@gmail.com>
2016-08-18 20:30:26 +0600

    Added slide in arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,21 @@
 var roboarm = document.getElementById('roboarm');
+var sign = document.getElementById('signUpBoard');
+var arrow = document.getElementById('arrow');
 
 window.onload = function() {
     roboarm.classList.add("animate");
     TweenMax.to("#arm", 1, {
-        rotation: 16,
+        rotation: 17,
         delay: 4,
-        transformOrigin: "20px 264px"
+        transformOrigin: "20px 264px",
+        onComplete: myCallback
     });
 
+
+
     new WOW().init();
+}
 
-
+function myCallback() {
+    
 }
