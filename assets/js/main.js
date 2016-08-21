221ed9e692ae961cf4e872288d94da552e1d1778
Rashed Doha <RashedDoha92@gmail.com>
2016-08-21 17:42:59 +0600

    Fixed animation on small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,27 @@ var sign = document.getElementById('signUpBoard');
 var arrow = document.getElementById('arrow');
 
 window.onload = function() {
-    roboarm.classList.add("animate");
-    TweenMax.to("#arm", 1, {
-        rotation: 17,
-        delay: 4,
-        transformOrigin: "20px 264px",
-        onComplete: myCallback
-    });
+    var width = $(window).width();
+    console.log(width);
+    if(width > 430)
+    {
+        roboarm.classList.add("animate");
+        TweenMax.to("#arm", 1, {
+            rotation: 17,
+            delay: 4,
+            transformOrigin: "20px 264px",
+            onComplete: myCallback
+        });
+    }
+    else {
+        TweenMax.to("#arm", 1, {
+            rotation: 17,
+            delay: 1,
+            transformOrigin: "20px 264px",
+            onComplete: myCallback
+        });
+    }
+
 
 
 
@@ -17,5 +31,5 @@ window.onload = function() {
 }
 
 function myCallback() {
-    
+
 }
